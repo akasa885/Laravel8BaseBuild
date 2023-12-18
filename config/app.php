8f0bfefabcf901a0ec433bc8b25640a95ac5ae6e
Rizki Akbar <akasa2444@gmail.com>
2023-12-18 11:11:43 +0700

    install: Barryvdh/debugbar into project

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -245,6 +246,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /*
+         * Package Aliases...
+         */
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
