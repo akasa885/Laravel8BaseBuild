d1a3843827b42729ffa5d75982810990478799ba
Rizki Akbar <akasa2444@gmail.com>
2023-12-18 10:42:50 +0700

    feat(localize): add localization component setting.
    - create middleware localization
    - kernel registration web. localization middeware
    - config app availbale locals

--- config/app.php
+++ config/app.php
@@ -1,7 +1,18 @@
 <?php
 
 return [
-
+    /**
+     * --------------------------------------------------------------------------
+     * Application Version
+     * --------------------------------------------------------------------------
+     * 
+     * This value is the version of your application. This value is used when the
+     * framework needs to place the application's version in a notification or
+     * any other location as required by the application or its packages.
+     * 
+     */
+    'version' => '1.1.0',
+    
     /*
     |--------------------------------------------------------------------------
     | Application Name
@@ -81,6 +92,11 @@ return [
     */
 
     'locale' => 'en',
+
+    'available_locales' => [
+        'Indonesia' => 'id',
+        'English' => 'en'
+    ],
 
     /*
     |--------------------------------------------------------------------------
