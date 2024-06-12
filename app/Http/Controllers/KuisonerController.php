cf800f465f9492dfe097b7cb724ca01b06012074
Rizki Akbar <akasa2444@gmail.com>
2024-06-12 15:43:39 +0700

    create: KuisonerController

---
+++ app/Http/Controllers/KuisonerController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\QuizSession;
+
+class KuisonerController extends Controller
+{
+    public function fill(Request $request, $token)
+    {
+        $quizSession = QuizSession::where('token', $token)->openQuiz()->firstOrFail();
+        $quizSession->load('quiz', 'quiz.questions');
+
+        return view('pages.quiz-page', 
+        [
+            'title' => 'Quiz - ' . $quizSession->quiz->title,
+        ],
+        compact('quizSession'));
+    }
+}
